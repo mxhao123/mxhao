29364826fa914e8faf8332d13f470d083b2f4a52
mxhao <273095492@qq.com>
2020-01-13 17:13:46 +0800

    ok

---
+++ src/main/java/com/fh/bean/ClassBean.java
@@ -0,0 +1,28 @@
+package com.fh.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName("t_class")
+public class ClassBean {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private String className;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
